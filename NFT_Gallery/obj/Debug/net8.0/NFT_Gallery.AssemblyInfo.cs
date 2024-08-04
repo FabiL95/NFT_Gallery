acb012ed1bb96f304a13fc2ce2a6642b8cc67a07
FabiL95 <ludwigerfabian@gmail.com>
2024-08-04 13:26:01 +0200

    Removed Privacy Tab in Navbar. Deleted Privacy Method in HomeController. Deleted Privacy ViewPage

--- NFT_Gallery/obj/Debug/net8.0/NFT_Gallery.AssemblyInfo.cs
+++ NFT_Gallery/obj/Debug/net8.0/NFT_Gallery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NFT_Gallery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c738cd7ac3980a561074642468688a25652d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("NFT_Gallery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NFT_Gallery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
