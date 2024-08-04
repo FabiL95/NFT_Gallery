d5c738cd7ac3980a561074642468688a25652d28
FabiL95 <ludwigerfabian@gmail.com>
2024-08-04 13:20:20 +0200

    NFT_Gallery Project created

---
+++ NFT_Gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NFT_Gallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
